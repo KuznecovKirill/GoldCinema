5bf27d71e9466077f068cdb74691f64a321dd8bc
Kirill <kirillkuznecov03@mail.ru>
2024-12-27 10:38:38 +0500

    Успешное добавление проектов, которые похожи на проект с id 301

--- server/src/models/PlayGround.js
+++ server/src/models/PlayGround.js
@@ -1,79 +1,91 @@
 const { Sequelize } = require("sequelize");
 const modelUser = require("./modelUser"); // Импортирт модели пользователя
-const { modelRewiew } = require("./modelRewiew"); 
-const swaggerUrl = require('../swagger/swagger.config');
-const {swaggerAPI} = require('../swagger/swagger.api');
-const {modelMedia} = require('./modelMedia');
-const {modelSimilar} = require('./modelSimilar')
-require('dotenv').config();
+const { modelRewiew } = require("./modelRewiew");
+const swaggerUrl = require("../swagger/swagger.config");
+const { swaggerAPI } = require("../swagger/swagger.api");
+const { modelMedia } = require("./modelMedia");
+const { modelSimilar } = require("./modelSimilar");
+require("dotenv").config();
 
 const sequelize = require("./database").sequelize;
 
 //Получение фильма по ID
-async function getMovie() {
-  const newMedia = await swaggerAPI.mediaByID({id: 301}); 
+async function addMovie(newMedia) {
+  // const newMedia = await swaggerAPI.mediaByID({ id: 301 });
   console.log(newMedia);
-  console.log(newMedia.countries);
-  await modelMedia.create({
-    id_media: newMedia.kinopoiskId,
-    title: newMedia.nameRu,
-    country: newMedia.countries.map(c => c.country).join(', '), //список фильмов
-    genre: newMedia.genres.map(g => g.genre).join(', '), //список жанров
-    running_time: newMedia.filmLength,
-    rars: `${newMedia.ratingAgeLimits.replace(/\D/g, '')}+`, //удаление всех нечисловых символов и добавление плюса на конце
-    rating: newMedia.ratingImdb,
-    descrition: newMedia.description,
-    poster: newMedia.posterUrlPreview
-  });
+  try {
+    await modelMedia.create({
+      id_media: newMedia.kinopoiskId,
+      title: newMedia.nameRu,
+      country: newMedia.countries.map((c) => c.country).join(", "), //список фильмов
+      genre: newMedia.genres.map((g) => g.genre).join(", "), //список жанров
+      running_time: newMedia.filmLength,
+      rars: `${newMedia.ratingAgeLimits.replace(/\D/g, "")}+`, //удаление всех нечисловых символов и добавление плюса на конце
+      rating: newMedia.ratingImdb,
+      descrition: newMedia.description,
+      poster: newMedia.posterUrlPreview,
+    });
+  } catch (error) {
+    if (error.name == "SequelizeUniqueConstraintError") {
+      console.log("Такой фильм уже существует!");
+    }
+  }
   sequelize.sync();
 }
 //Получение списка фильмов
 async function getMovies() {
-  const newCollection = await swaggerAPI.mediaCollections({type:'TOP_POPULAR_ALL', page: '1'});
+  const newCollection = await swaggerAPI.mediaCollections({
+    type: "TOP_POPULAR_ALL",
+    page: "1",
+  });
   newCollection.items.forEach(async (item) => {
-    if (item.nameRu !== null){
+    if (item.nameRu !== null) {
       console.log(item.nameRu);
-    try{
-    await modelMedia.create({
-        id_media: item.kinopoiskId,
-        title: item.nameRu,
-        country: item.countries.map(c => c.country).join(', '),
-        genre: item.genres.map(g => g.genre).join(', '),
-        running_time: item.filmLength || null,
-        rars: item.ratingAgeLimits ? `${item.ratingAgeLimits.replace(/\D/g, '')}+` : null,
-        rating: item.ratingImdb || null,
-        descrition: item.description || null,
-        poster: item.posterUrlPreview || null
-    });
-  }
-  catch (error){
-    if (error.name == 'SequelizeUniqueConstraintError' ){
-      console.log('Такой фильм уже существует!');
+      try {
+        await modelMedia.create({
+          id_media: item.kinopoiskId,
+          title: item.nameRu,
+          country: item.countries.map((c) => c.country).join(", "),
+          genre: item.genres.map((g) => g.genre).join(", "),
+          running_time: item.filmLength || null,
+          rars: item.ratingAgeLimits
+            ? `${item.ratingAgeLimits.replace(/\D/g, "")}+`
+            : null,
+          rating: item.ratingImdb || null,
+          descrition: item.description || null,
+          poster: item.posterUrlPreview || null,
+        });
+      } catch (error) {
+        if (error.name == "SequelizeUniqueConstraintError") {
+          console.log("Такой фильм уже существует!");
+        }
+      }
     }
-  }
-  }
-});
-sequelize.sync();
-}
-async function checkID(id) {
-  
-  
-}
-async function getSimilars() {
-  const newSimilar = await swaggerAPI.mediaSimilars({id: '301/'}, {similars: 'similars'});
-  console.log(newSimilar);
-  const firstSixMovies = newSimilar.items.slice(0, 6);
-  for (const item of firstSixMovies){
-    if (item.nameRu !== null){
-    await modelSimilar.create({
-      id_media: item.filmId || null,
-      title: item.nameRu,
-      poster: item.posterUrlPreview
-    });
-  }
-} 
+  });
   sequelize.sync();
 }
-//getSimilars();
-getMovies();
+async function checkID(id) {}
+async function getSimilars() {
+  const newSimilar = await swaggerAPI.mediaSimilars(
+    { id: "301/" },
+    { similars: "similars" }
+  );
+  console.log(newSimilar);
+  const firstSixMovies = newSimilar.items.slice(0, 6);
+  for (const item of firstSixMovies) {
+    if (item.nameRu !== null) {
+      const newMedia = await swaggerAPI.mediaByID({ id: item.filmId });
+      addMovie(newMedia);
+      await modelSimilar.create({
+        id_origin: 301,
+        id_media: item.filmId || null,
+        title: item.nameRu,
+        poster: item.posterUrlPreview,
+      });
+    }
+  }
+  sequelize.sync();
+}
+getSimilars();
+//getMovies();
 // getMovie();
--- server/src/models/modelSimilar.js
+++ server/src/models/modelSimilar.js
@@ -1,48 +1,55 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const {modelMedia} = require('./modelMedia');
-const sequelize = require('./database').sequelize;
+const { Sequelize, DataTypes } = require("sequelize");
+const { modelMedia } = require("./modelMedia");
+const sequelize = require("./database").sequelize;
 
 //Модель похожих проектов
-const modelSimilar = sequelize.define('Similar', {
+const modelSimilar = sequelize.define(
+  "Similar",
+  {
     id_similar: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    id_origin: {
+      type: DataTypes.INTEGER,
     },
     id_media: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: modelMedia,
-            key: 'id_media'
-        }
+      type: DataTypes.INTEGER,
+      references: {
+        model: modelMedia,
+        key: "id_media",
       },
-    title:{
-        type: DataTypes.STRING,
-        allowNull: false
     },
-    poster:{
-        type: DataTypes.STRING,
-        allowNull: true
-    }
-},
-    {
-    timestamps: false, 
-    freezeTableName: true, 
-});
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    poster: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    timestamps: false,
+    freezeTableName: true,
+  }
+);
 
-modelSimilar.prototype.toObject = function() {
-    const values = { ...this.get() };
-    return values;
+modelSimilar.prototype.toObject = function () {
+  const values = { ...this.get() };
+  return values;
 };
 
 // Преобразование в JSON
-modelSimilar.prototype.toJSON = function() {
-    const values = { ...this.get() };; 
-    return values;
+modelSimilar.prototype.toJSON = function () {
+  const values = { ...this.get() };
+  return values;
 };
 (async () => {
-    // Синхронизация моделей с базой данных без удаления существующих данных
-    await sequelize.sync();
-  })();
+  // Синхронизация моделей с базой данных без удаления существующих данных
+  //await sequelize.sync();
+  await sequelize.sync({ alter: true });
+})();
 module.exports = { modelSimilar };
