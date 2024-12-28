53676b396aee966c74d19b323cf7a49ab27740c1
Kirill <kirillkuznecov03@mail.ru>
2024-12-28 11:20:34 +0500

    Создание файла для валидации запросов в handlers

---
+++ server/src/handlers/request.handler.js
@@ -0,0 +1,16 @@
+//Валидация входяхищ запросов на основе Express.js
+const validationResult = require('express-validator');
+
+//req - объект запроса, res - объект ответа, next - функция для передачи следующему middleware
+const validate = (req, res, next) => {
+    const errors = validationResult(req); //Получение списка ошибок валидации
+  
+    
+    if (!errors.isEmpty()) return res.status(400).json({
+      message: errors.array()[0].msg
+    });
+  
+    next();
+  };
+  
+  export default { validate };
