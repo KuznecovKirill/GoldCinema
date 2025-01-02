a948ac840783f9dc850cd62104cb64a91dca380e
Kirill <kirillkuznecov03@mail.ru>
2025-01-02 12:01:11 +0500

    Добавление функции Входа

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -8,32 +8,62 @@ const crypto = require('crypto');
 const signUp = async (req, res) => {
   try {
     const { us, pass } = req.body;
-    const checkUser = await modelUser.findOne({ where: { username: "newuser" } }); //поиск пользователя с таким именем
-
+    console.log(us);
+    const checkUser = await modelUser.findOne({ where: {username: us} }); //поиск пользователя с таким именем
     //curl -X POST -H "Content-Type: application/json" -d '{"us": "newuser", "pass": "password123"}' http://localhost:8000/signUp
 
     if (checkUser) return responseHandler.badrequest(res, "Такой пользователь уже зарегестрирован");
     const user = await modelUser.create({
       username: us,
-      password: pass, // Предполагается, что метод setPassword будет обрабатывать хеширование пароля
-      salt: crypto.randomBytes(16).toString("hex")
+      password: pass,
+      passToken: crypto.randomBytes(16).toString("hex")
     });
     sequelize.sync();
     console.log(us);
 
     const token = jsonwebtoken.sign(
-      { data: user.id },
+      { data: user.id_user },
       process.env.TOKEN_SECRET,
       { expiresIn: "24h" }
     );
 
+    //Отправка успешного ответа
     responseHandler.created(res, {
       token,
       ...user.dataValues,
-      id: user.id,
+      id: user.id_user,
     });
   } catch (err) {
-    console.log(err);
+    // console.log(err);
+    responseHandler.error(res);
+  }
+};
+//Вход
+const signIn = async (req, res) => {
+  try {
+    const { us, pass } = req.body;
+
+    const user = await modelUser.findOne({ where: {username: us} }).select("id_user username password passToken");
+
+    if (!user) return responseHandler.badrequest(res, "Такого пользователя не существует!"); //Проверка пользователя
+
+    if (!user.validPassword(pass)) return responseHandler.badrequest(res, "Неверный пароль");
+
+    const token = jsonwebtoken.sign(
+      { data: user.id_user },
+      process.env.TOKEN_SECRET,
+      { expiresIn: "24h" }
+    );
+
+    user.password = undefined;
+    user.passToken = undefined;
+
+    responseHandler.created(res, {
+      token,
+      ...user._doc,
+      id: user.id_user
+    });
+  } catch {
     responseHandler.error(res);
   }
 };
--- server/src/models/modelUser.js
+++ server/src/models/modelUser.js
@@ -1,78 +1,74 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = require('./database').sequelize;
-const crypto = require('crypto');
+const { Sequelize, DataTypes, Model } = require("sequelize");
+const sequelize = require("./database").sequelize;
+const crypto = require("crypto");
 
 //Модель пользователя
-const modelUser = sequelize.define('User', {
+const modelUser = sequelize.define(
+  "User",
+  {
     id_user: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
     },
     username: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        select: false 
+      type: DataTypes.STRING,
+      allowNull: false,
+      select: false,
     },
-    salt: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        select: false 
-    }
-}, {
-    timestamps: false, 
-    freezeTableName: true, 
-});
+    passToken: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      select: false,
+    },
+  },
+  {
+    timestamps: false,
+    freezeTableName: true,
+  }
+);
 
 // Метод для установки пароля
-modelUser.prototype.setPassword = function(password) {
-    this.salt = crypto.randomBytes(16).toString("hex");
+modelUser.prototype.setPassword = function (password) {
+  this.passToken = crypto.randomBytes(16).toString("hex");
 
-    this.password = crypto.pbkdf2Sync(
-        password,
-        this.salt,
-        1000,
-        64,
-        "sha512"
-    ).toString("hex");
-    console.log(this.salt);
+  this.password = crypto
+    .pbkdf2Sync(password, this.passToken, 1000, 64, "sha512")
+    .toString("hex");
+  console.log(this.passToken);
 };
 
 // Метод для проверки пароля
-modelUser.prototype.validPassword = function(password) {
-    const hash = crypto.pbkdf2Sync(
-        password,
-        this.salt,
-        1000,
-        64,
-        "sha512"
-    ).toString("hex");
+modelUser.prototype.validPassword = function (password) {
+  const hash = crypto
+    .pbkdf2Sync(password, this.passToken, 1000, 64, "sha512")
+    .toString("hex");
 
-    return this.password === hash;
+  return this.password === hash;
 };
 
-modelUser.prototype.toObject = function() {
-    const values = { ...this.get() };
-    delete values.id_user; // Исправлено на id_user
-    return values;
+modelUser.prototype.toObject = function () {
+  const values = { ...this.get() };
+  delete values.id_user; // Исправлено на id_user
+  return values;
 };
 
 // Преобразование в JSON
-modelUser.prototype.toJSON = function() {
-    const values = { ...this.get() };
-    // delete values.id_user; 
-    return values;
+modelUser.prototype.toJSON = function () {
+  const values = { ...this.get() };
+  // delete values.id_user;
+  return values;
 };
 
 (async () => {
-    // Синхронизация моделей с базой данных без удаления существующих данных
-    await sequelize.sync({ alter: true });
-  })();
+  // Синхронизация моделей с базой данных без удаления существующих данных
+  await sequelize.sync({ alter: true });
+})();
 // (async () => {
 //     await modelUser.sync(); // Синхронизация таблицы
 
