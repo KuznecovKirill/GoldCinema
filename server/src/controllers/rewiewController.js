2cf7ddd8f62107d27de70b853320d082f8c62733
Kirill <kirillkuznecov03@mail.ru>
2025-01-10 09:45:18 +0500

    Создание роутера для отзывов

--- server/src/controllers/rewiewController.js
+++ server/src/controllers/rewiewController.js
@@ -1,67 +1,65 @@
-const responseHandler = require('../handlers/response.handler');
-const modelReview = require('../models/modelReview');
+const responseHandler = require("../handlers/response.handler");
+const modelReview = require("../models/modelReview");
 const sequelize = require("../models/database").sequelize;
 const create = async (req, res) => {
-    try {
-      const { id_media, rating, comment } = req.body;
+  try {
+    const { id_media, rating, comment } = req.body;
 
-      const review = await modelReview.create({
-        id_user: user.id_user,
-        id_media: id_media,
-        rating_user: rating,
-        comment_text: comment
-      });
-      sequelize.sync();
-  
-  
-      responseHandler.created(res, {
-        id: review.id_review,
-        user: req.user,
-        ...review.dataValues
-      });
-    } catch (error) {
-        console.error("Ошибка:", error);
-        responseHandler.error(res);
-    }
-  };
-  const remove = async (req, res) => {
-    try {
-      const { id_review } = req.params;
-  
-      // Поиск отзыва по ID и пользователю
-      const review = await modelReview.findOne({
-        where: {
-          id: id_review,
-          id_user: req.user.id // Проверяем, что отзыв принадлежит текущему пользователю
-        }
-      });
-  
-      if (!review) return responseHandler.notfound(res); // Если отзыв не найден, отправляем 404
-  
-      await review.destroy(); // Удаление отзыва
-  
-      responseHandler.goodrequest(res); // Отправка успешного ответа
-    } catch (error) {
-      console.error("Ошибка:", error);
-      responseHandler.error(res);
-    }
-  };
+    const review = await modelReview.create({
+      id_user: user.id_user,
+      id_media: id_media,
+      rating_user: rating,
+      comment_text: comment,
+    });
+    sequelize.sync();
 
-  const getReviewsOfUser = async (req, res) => {
-    try {
-      // Получаем все отзывы пользователя, сортируя их по дате создания
-      const reviews = await modelReview.findAll({
-        where: {
-          id_user: req.user.id
-        },
-        order: [["createdAt", "DESC"]] // Сортировка по дате создания в порядке убывания
-      });
-  
-      responseHandler.goodrequest(res, reviews); // Отправляем успешный ответ с отзывами
-    } catch (error) {
-      console.error("Ошибка:", error); 
-      responseHandler.error(res); 
-    }
-  };
-  module.exports = {create, remove, getReviewsOfUser};
-  
+    responseHandler.created(res, {
+      id: review.id_review,
+      user: req.user,
+      ...review.dataValues,
+    });
+  } catch (error) {
+    console.error("Ошибка:", error);
+    responseHandler.error(res);
+  }
+};
+const remove = async (req, res) => {
+  try {
+    const { id_review } = req.params;
+
+    // Поиск отзыва по ID и пользователю
+    const review = await modelReview.findOne({
+      where: {
+        id: id_review,
+        id_user: req.user.id, // Проверяем, что отзыв принадлежит текущему пользователю
+      },
+    });
+
+    if (!review) return responseHandler.notfound(res); // Если отзыв не найден, отправляем 404
+
+    await review.destroy(); // Удаление отзыва
+
+    responseHandler.goodrequest(res); // Отправка успешного ответа
+  } catch (error) {
+    console.error("Ошибка:", error);
+    responseHandler.error(res);
+  }
+};
+
+const getReviewsOfUser = async (req, res) => {
+  try {
+    // Получаем все отзывы пользователя, сортируя их по дате создания
+    const reviews = await modelReview.findAll({
+      where: {
+        id_user: req.user.id,
+      },
+      order: [["createdAt", "DESC"]], // Сортировка по дате создания в порядке убывания
+    });
+
+    responseHandler.goodrequest(res, reviews); // Отправляем успешный ответ с отзывами
+  } catch (error) {
+    console.error("Ошибка:", error);
+    responseHandler.error(res);
+  }
+};
+module.exports = { create, remove, getReviewsOfUser };
--- server/src/routes/rewiewRoute.js
+++ server/src/routes/rewiewRoute.js
@@ -1,6 +1,28 @@
+const express = require("express");
+const validator = require("express-validator");
+const reviewController = require("../controllers/rewiewController");
+const tokenMiddleware = require("../middlewares/middleware");
+const requestHandler = require("../handlers/request.handler");
 
-const express = require('express');
-const validator = require('express-validator');
-const reviewController = require('../controllers/...');
-const tokenMiddleware  = require('../middlewares/middleware');
-const requestHandler = require('../handlers/request.handler');
+const router = express.Router();
+
+router.get("/", tokenMiddleware.user, reviewController.getReviewsOfUser);
+
+router.post(
+  "/",
+  tokenMiddleware.user,
+  body("id_media")
+    .exists()
+    .withMessage("Требуется id_media")
+    .isLength({ min: 1 })
+    .withMessage("mediaId не может быть пустым"),
+  body("rating_user")
+    .exists()
+    .withMessage("Требуется рейтинг"),
+  body("comment_text")
+    .exists()
+    .withMessage("Требуется комментарий"),
+  requestHandler.validate,
+  reviewController.create
+);
+module.exports = {router};
