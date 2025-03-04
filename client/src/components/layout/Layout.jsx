45d92f9fff95e7b3df5d5217612a9a56fc5c5fc6
Kirill <kirillkuznecov03@mail.ru>
2025-03-04 20:10:10 +0500

    Теперь приложение сохраняет данные пользователя при перезагрузке и ему не нужно повторно производить вход в аккаунт

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -6,7 +6,42 @@ import GlobalLoading from '../common/GlobalLoading';
 import Footer from '../common/Footer';
 import Topbar from '../common/Topbar';
 import AuthModal from '../common/AuthModal';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
+import userModule from '../../api/modules/userModule';
+import favoriteModule from "../../api/modules/favoriteModule";
+import { setListFavorites, setUser } from '../../redux/slices/userSlice';
 const Layout = () => {
+  //Нужно сделать так, чтобы при перезагрузке пользователь не выходил из системы
+  //Состояния пользователя нужно получать через JWT
+  const dispatch = useDispatch();
+  const {user} = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const authUser = async () => {
+      const { response, err } = await userModule.getInfo();
+
+      if (response) dispatch(setUser(response));
+      if (err) dispatch(setUser(null));
+    };
+
+    authUser();
+  }, [dispatch]);
+
+  // useEffect(() => {
+  //   const getFavorites = async () => {
+  //     const { response, err } = await favoriteModule.getList();
+
+  //     if (response) dispatch(setListFavorites(response));
+  //     if (err) toast.error(err.message);
+  //   };
+
+  //   if (user) getFavorites();
+  //   if (!user) dispatch(setListFavorites([]));
+  // }, [user, dispatch]);
+
+
   return (
     <>
     <GlobalLoading />
