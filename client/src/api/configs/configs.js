df18702007949e4fd09da7758aeeea675d404943
Kirill <kirillkuznecov03@mail.ru>
2025-03-06 09:54:42 +0500

    Закоменчен лишний текст. Позже будет удалён

--- client/src/api/configs/configs.js
+++ client/src/api/configs/configs.js
@@ -1,5 +1,4 @@
-import { modelMedia } from "../../../../server/src/models/modelMedia";
-import { search } from "../../../../server/src/routes/userRoute";
+
 
 const mediaType = {
     FILM: "FILM",
@@ -15,29 +14,30 @@ const mediaType = {
     search: "search"
   };
 
-  const getMedia = async (type) => {
-    try {
-      const mediaItems = await modelMedia.findAll({
-        where: {
-          mediaType: type
-        }
-      });
-      return mediaItems.map(media => ({
-        ...media.toObject(),
-        backdrop: backdropPath(media.poster), 
-        poster: posterPath(media.poster)
-      }));
-    } catch (error) {
-      console.error("Ошибка при получении медиа:", error);
-      throw error;
-    }
-  };
+  // const getMedia = async (type) => {
+  //   try {
+  //     const mediaItems = await modelMedia.findAll({
+  //       where: {
+  //         mediaType: type
+  //       }
+  //     });
+  //     return mediaItems.map(media => ({
+  //       ...media.toObject(),
+  //       backdrop: backdropPath(media.poster), 
+  //       poster: posterPath(media.poster)
+  //     }));
+  //   } catch (error) {
+  //     console.error("Ошибка при получении медиа:", error);
+  //     throw error;
+  //   }
+  // };
   const backdropPath = (media_id) => `https://kinopoiskapiunofficial.tech/images/posters/kp_small/${media_id}.jpg`;
   const posterPath = (media_id) => `https://kinopoiskapiunofficial.tech/images/posters/kp_small/${media_id}.jpg`;
   const configs = {
     mediaType,
     mediaCategory,
-    getMedia
+    backdropPath,
+    posterPath
   };
   
   export default configs;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { restoreUserState } from './redux/slices/userSlice';
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+
+
+// const RootComponent = () => {
+//   useEffect(() => {
+//     const restoreState = async () => {
+//       await store.dispatch(restoreUserState());
+//     };
+//     restoreState();
+//   }, []);
+
+//   return (
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   );
+// };
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   // <React.StrictMode>
+//   <RootComponent />
+//   // </React.StrictMode>
+// );
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
