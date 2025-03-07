c010d25fb5d91e80a099d445066d8d04d077ab01
Kirill <kirillkuznecov03@mail.ru>
2025-03-07 13:16:20 +0500

    Создание MainSlide для отображения слайдов медиа. Получается неплохо, но для экранов lg выглядит не очень

---
+++ client/src/components/common/MainSlide.jsx
@@ -0,0 +1,121 @@
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import { Box, Button, Chip, Divider, Stack, Typography, useTheme } from "@mui/material";
+
+import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Autoplay } from 'swiper/modules';
+
+import { toast } from "react-toastify";
+
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+
+import {setGlobalLoading} from "../../redux/slices/globalLoadingSlice";
+import {routeGen} from "../../routes/routes";
+import uiConfigs from "../../configs/UI";
+import CircleRate from "./CircleRate";
+import configs from "../../api/configs/configs";
+
+import genreModule from "../../api/modules/genreModule";
+import mediaModule from "../../api/modules/mediaModule";
+const MainSlide = ({mediaType}) => {
+    const theme = useTheme();
+    const dispatch = useDispatch();
+  
+    const [medias, setMedias] = useState([]);
+    const [genres, setGenres] = useState([]);
+
+    useEffect(() => {
+        const getMedias = async () => {
+          const { response, err } = await mediaModule.getMedias({
+            mediaType,
+            page: 1,
+            limit: 10
+          });
+          if (response){ 
+            console.log(response.medias);
+            setMedias(response.medias);
+          }
+          if (err) toast.error(err.message);
+          dispatch(setGlobalLoading(false));
+    };
+    const getGenres = async () => {
+        dispatch(setGlobalLoading(true));
+        const { response, err } = await genreModule.getList({ mediaType });
+  
+        if (response) {
+          setGenres(response.genres);
+          getMedias();
+        }
+        if (err) {
+          toast.error(err.message);
+          setGlobalLoading(false);
+        }
+      };
+  
+      getGenres();
+    }, [mediaType, dispatch]);
+    
+    return (
+        <Box sx={{
+            position: "relative",
+            color: "primary.contrastText",
+            "&::before": {
+              content: '""',
+              width: "100%",
+              height: "30%",
+              position: "absolute",
+              bottom: 0,
+              left: 0,
+              zIndex: 2,
+              pointerEvents: "none",
+              ...uiConfigs.style.gradientBgImage[theme.palette.mode]
+            }
+          }}>
+            <Swiper 
+            grabCursor={true}
+            loop={true}
+            // modules={[Autoplay]}
+            style={{width: "100%", height: "max-content"}}
+            // autoplay={{
+            //   delay: 3000,
+            //   disableOnInteraction: false
+            // }}
+            >
+              {medias.map((med, index) => (
+                <SwiperSlide key={index}>
+                  <Box sx={{
+                  //   height: { // Используем фиксированную высоту
+                  //     xs: '200px',
+                  //     sm: '300px',
+                  //     md: '400px',
+                  //     lg: '500px'
+                  // },
+                  // backgroundPosition: "center", // Выравниваем изображение по центру
+                  // backgroundSize: "contain", // Изменяем размер изображения, сохраняя пропорции
+                  // backgroundRepeat: "no-repeat", // Предотвращаем повторение изображения
+                    paddingTop: {
+                      xs: "110%",
+                      sm: "70%",
+                      md: "50%",
+                      lg: "35%",
+                      '@media (min-width: 1200px)': { 
+                        paddingTop: '40%',},
+                    },
+                    overflow: 'hidden',
+                    backgroundPosition: "top",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    // backgroundImage: `url(${configs.backdropPath(med.id_media)})`,
+                    backgroundImage: `url(${med.cover})`,
+                  }}>
+
+                  </Box>
+
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </Box>
+    );
+};
+export default MainSlide;
