4356bbfb93dcd11ff2facff000b7aeb9f10872e6
Kirill <kirillkuznecov03@mail.ru>
2025-03-10 14:02:57 +0500

    Горизонтальное затемнение слайдов

--- client/src/configs/UI.js
+++ client/src/configs/UI.js
@@ -8,6 +8,14 @@ const uiConfigs = {
           backgroundImage: "linear-gradient(to top, rgba(245,245,245,1), rgba(0,0,0,0))"
         }
       },
+      horizontalGradientBgImage: {
+        dark: {
+          backgroundImage: "linear-gradient(to right, rgba(0,0,0,1), rgba(0,0,0,0))"
+        },
+        light: {
+          backgroundImage: "linear-gradient(to right, rgba(245,245,245,1), rgba(0,0,0,0))"
+        }
+      },
       typoLines: (lines, textAlign) => ({ //для обрезки многострочного текста
         textAlign: textAlign || "justify",
         display: "-webkit-box",
