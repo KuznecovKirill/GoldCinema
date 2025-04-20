f4bea144d62edb251c3d3a6a6ff72cf331544747
Kirill <kirillkuznecov03@mail.ru>
2025-04-20 13:02:41 +0500

    Улучшена страница с поиском. Теперь отображаются типы медиа на русском

--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -5,55 +5,61 @@ import UI from "../configs/UI";
 import MediaGrid from "../components/common/MediaGrid";
 import { Box, Button, Stack, TextField, Toolbar } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
-const mediaTypes = ["FILM", "TV_SERIES", "ALL"];
+
+// Опции для разделения английских и русских слов
+const mediaTypeOptions = [
+  { key: "FILM", label: "Фильм" },
+  { key: "TV_SERIES", label: "Сериал" },
+  { key: "ALL", label: "Все" }
+];
+
 let timer;
 const timeOut = 1000;
+
 const SearchPage = () => {
   const [searchM, setSearchM] = useState(false);
-  const [mediaType, setMediaType] = useState(mediaTypes[0]);
+  const [mediaType, setMediaType] = useState(mediaTypeOptions[0].key); // Используется ключ
   const [query, setQuerry] = useState("");
   const [medias, setMedias] = useState([]);
   const [page, setPage] = useState(1);
 
   const search = useCallback(async () => {
-
     setSearchM(true);
-    
     const { response, err } = await mediaModule.search({
       mediaType,
       query,
       page,
     });
-    console.log("Поиск начат");
-    console.log(response);
     setSearchM(false);
+    
     if (err) toast.error(err.message);
     if (response) {
-      console.log(response);
-      if (page > 1) setMedias((med) => [...med, ...response]);
-      else setMedias([...response]);
+      page > 1 ? setMedias((med) => [...med, ...response]) : setMedias([...response]);
     }
   }, [mediaType, query, page]);
+
   useEffect(() => {
     if (query.trim().length === 0) {
       setMedias([]);
       setPage(1);
-    } else search();
+    } else {
+      search();
+    }
   }, [search, query, mediaType, page]);
+
   useEffect(() => {
     setMedias([]);
     setPage(1);
   }, [mediaType]);
 
   const categoryChange = (cat) => setMediaType(cat);
-  const queryChange = (q) => {
-    const newQuerry = q.target.value;
+  
+  const queryChange = (e) => {
+    const newQuery = e.target.value;
     clearTimeout(timer);
-
-    timer = setTimeout(() => {
-      setQuerry(newQuerry);
-    }, timeOut);
+    timer = setTimeout(() => setQuerry(newQuery), timeOut);
   };
+
   return (
     <>
       <Toolbar />
@@ -65,23 +71,23 @@ const SearchPage = () => {
             justifyContent="center"
             sx={{ width: "100%" }}
           >
-            {mediaTypes.map((item, index) => (
+            {mediaTypeOptions.map((item) => (
               <Button
                 size="large"
-                key={index}
-                variant={mediaType === item ? "contained" : "text"}
+                key={item.key}
+                variant={mediaType === item.key ? "contained" : "text"}
                 sx={{
-                  color:
-                    mediaType === item
-                      ? "primary.contrastText"
-                      : "text.primary",
+                  color: mediaType === item.key 
+                    ? "primary.contrastText" 
+                    : "text.primary",
                 }}
-                onClick={() => categoryChange(item)}
+                onClick={() => categoryChange(item.key)}
               >
-                {item}
+                {item.label} {/* Отображение типов на русском*/}
               </Button>
             ))}
           </Stack>
+          
           <TextField
             color="success"
             placeholder="Search GoldCinema"
@@ -96,7 +102,7 @@ const SearchPage = () => {
 
           {medias.length > 0 && (
             <LoadingButton loading={searchM} onClick={() => setPage(page + 1)}>
-              загрузить ещё
+              Загрузить ещё
             </LoadingButton>
           )}
         </Stack>
