958e3d73230caac195b24f952c917ae322477adb
Kirill <kirillkuznecov03@mail.ru>
2026-01-13 18:15:09 +0500

    Теперь точно Grid починил

--- client/src/components/common/MediaGrid.jsx
+++ client/src/components/common/MediaGrid.jsx
@@ -1,35 +1,18 @@
-import { Box } from '@mui/material';
-import React from 'react';
-import MediaItem from './MediaItem';
+import Grid from '@mui/material/GridLegacy';
+import React from 'react'
+import MediaItem from './MediaItem'
 
-const MediaGrid = ({ medias, mediaType }) => {
+const MediaGrid = ({medias, mediaType}) => {
+  console.log(medias);
   return (
-    <Box sx={{ 
-      display: 'grid', 
-      gridTemplateColumns: {
-        xs: 'repeat(2, 1fr)',      // 2 на мобиле <600px
-        sm: 'repeat(3, 1fr)',      // 3 на планшете 600-900px  
-        md: 'repeat(4, 1fr)',      // 4 на малом десктопе 900-1200px
-        lg: 'repeat(6, 1fr)',      // 6 на большом экране >1200px
-      },
-      gap: 1.2,
-      p: { xs: 0.5, md: 1.5 },  // Меньше отступы на мобиле
-      maxWidth: '100%'
-    }}>
-      {medias.map((media, index) => (
-        <Box 
-          key={media.id_media || index} 
-          sx={{ 
-            width: '100%', 
-            height: { xs: 200, sm: 220, md: 240, lg: 260 },  // Высота растёт с экраном
-            '& > *': { width: '100%', height: '100%' }
-          }}
-        >
-          <MediaItem media={media} mediaType={mediaType} />
-        </Box>
-      ))}
-    </Box>
-  );
-};
+    <Grid container spacing={1} sx={{marginRight: "-8px!important"}}>
+        {medias.map((med) => (
+            <Grid item xs={6} sm={4} md={3} key={med.id_media}>
+                <MediaItem media={med} mediaType={mediaType} />
+            </Grid>
+        ))}
+    </Grid>
+  )
+}
 
-export default MediaGrid;
+export default MediaGrid
