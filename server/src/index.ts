bb20ab281e760a5a87196bd978ae08f764b14d8b
Kirill <kirillkuznecov03@mail.ru>
2026-02-07 13:35:34 +0500

    index.js переделан под ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,8 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import http from "http";
 import routes from './routes/routes';
+import {NestFactory} from "@nestjs/core";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger"
 
 import userController from './controllers/userController';
 
@@ -16,52 +18,81 @@ import favoriteController from './controllers/favoriteController';
 import keywordController from './controllers/keywordController';
 
 import { setupAssociations } from './models/associations';
+import { AppModule } from './app.module';
+import { ErrorMiddleware } from './middlewares/errorMiddleware';
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+
+    app.setGlobalPrefix("/goldcinema/v1");
+
+    app.enableCors();
+
+    app.useGlobalFilters(new ErrorMiddleware());
+
+    const config = new DocumentBuilder()
+    .setTitle("GoldCinema")
+    .setDescription("Фуллстек для приложения GoldCinema")
+    .setVersion("2.0.0")
+    .addBearerAuth()
+    .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api/swagger", app, document);
+
+    const PORT = process.env.PORT || 8000;
+    console.log(process.env.PORT);
+
+    await app.listen(PORT)
+
+    console.log(`Server is running on ${PORT}`);
+}
+await bootstrap();
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(bodyParser.json());
+
+// app.use("/goldcinema/v1", routes);
+// ///goldcinema/v1
+
+// const port = process.env.PORT || 8000;
+// console.log(process.env.PORT);
 
 
-const app = express();
+// const server = http.createServer(app);
 
-app.use(cors());
-app.use(express.json());
-app.use(cookieParser());
-app.use(bodyParser.json());
+// //Создание коннекта
+// const connection = mysql.createConnection({
+//     host: 'MySQL-8.0',
+//     user: "root",
+//     database: "Gold_Cinema",
+//     password: "",
+// });
 
-app.use("/goldcinema/v1", routes);
-///goldcinema/v1
+// app.post('/', (req, res) => {
+//     res.send('POST запрос на корневом маршруте');
+// });
 
-const port = process.env.PORT || 8000;
-console.log(process.env.PORT);
-
-const server = http.createServer(app);
-
-//Создание коннекта
-//TODO: Нужно будет сделать dbConfig перед этим
-const connection = mysql.createConnection({
-    host: 'MySQL-8.0',
-    user: "root",
-    database: "Gold_Cinema",
-    password: "",
-});
-
-app.post('/', (req, res) => {
-    res.send('POST запрос на корневом маршруте');
-});
-
-//Подключение
-connection.connect(err => {
-    if (err) {
-        return console.error('Ошибка подключения: ' + err.message);
-    }
-    console.log('Подключение к серверу MySQL успешно установлено');
-    setupAssociations();
-    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+// //Подключение
+// connection.connect(err => {
+//     if (err) {
+//         return console.error('Ошибка подключения: ' + err.message);
+//     }
+//     console.log('Подключение к серверу MySQL успешно установлено');
+//     setupAssociations();
+//     console.log(`Server is running on http://localhost:${process.env.PORT}`);
     
-    server.listen(port, () => {
-        console.log(`Сервер слушает порт ${port}`);
-    });
-});
+//     server.listen(port, () => {
+//         console.log(`Сервер слушает порт ${port}`);
+//     });
+// });
 
-export {app, connection};
+// export {app, connection};
 
-//connection.end();
+// //connection.end();
 
 
