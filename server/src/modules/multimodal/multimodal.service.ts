0d72836b4f49d604ab168331d74875166aa14ca9
Kirill <kirillkuznecov03@mail.ru>
2026-04-28 13:23:38 +0500

    Метод для отправки запроса на компьютерное зрение

--- server/src/modules/multimodal/multimodal.service.ts
+++ server/src/modules/multimodal/multimodal.service.ts
@@ -1,6 +1,7 @@
 import { DrizzleService } from "@/database/drizzle.service";
 import { HttpServer, Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
+import axios from "axios";
 
 @Injectable()
 export class MultimodalService {
@@ -16,5 +17,32 @@ export class MultimodalService {
 
   private get db() {
     return this.drizzleService.db;
+  }
+
+  //Компьютерное зрение
+  async askImage(image: string, prompt: string): Promise<string> {
+    try {
+      const base64Data = image.includes("base64,")
+        ? image.split(",")[1]
+        : image;
+
+      const payload = {
+        model: "qwen2:7b", //Qwen2 - мультимодальный
+        prompt: prompt,
+        images: [base64Data],
+        stream: false,
+      };
+      const response = await axios.post<{ response: string }>(
+        `${this.apiUrl}/api/generate`,
+        payload,
+        {
+          headers: { "Content-Type": "application/json" },
+        },
+      );
+      return response.data.response;
+    } catch (error) {
+      console.log(error);
+      return "";
+    }
   }
 }
